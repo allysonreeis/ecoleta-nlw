fac629a11e6be49ec509a8071996519030061396
Allyson Reis <allysonreis1@gmail.com>
2020-06-05 16:52:08 -0300

    Finalizando NLW - 2020

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const db = require('./database/db');
 
 const server = express();
 
 server.use(express.static('public'));
+server.use(express.urlencoded({ extended: true }));
 
 nunjucks.configure('src/views', {
   express: server,
@@ -11,15 +13,73 @@ nunjucks.configure('src/views', {
 })
 
 server.get('/', (req, res) => {
-  return res.render('index.html', { title: "Seu marketplace de coleta de resíduos" });
+  return res.render('index.html', { title: "Um título" });
 });
 
 server.get('/create-point', (req, res) => {
+
   return res.render('create-point.html');
 });
 
+server.post('/savepoint', (req, res) => {
+  
+    const query = `
+    INSERT INTO places (
+      image, 
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `
+  
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertData (err) {
+    if (err) {
+      console.log(err);
+      return res.send('Erro no Cadastro');
+    }
+
+    console.log("Cadastrado com sucesso");
+    console.log(this);
+
+    return res.render('create-point.html', {saved: true});
+  }
+
+  db.run(query, values, afterInsertData);
+
+})
+
 server.get('/search', (req, res) => {
-  return res.render('search-results.html');
+
+  const search = req.query.search;
+
+  if (search == "") {
+    return res.render('search-results.html', { total: 0 });
+
+  }
+
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+    if (err){
+      return console.log(err);
+    }
+
+    const total = rows.length;
+
+    return res.render('search-results.html', { places: rows, total });
+  })
+
 });
 
 server.listen('3000');
